a25c743b448ae0aa4faf70e4a5f82bae5f946276
Alonso Decarli <alonso.decarli@viavarejo.com.br>
2025-08-15 12:59:07 -0300

    Adicionando o código fonte

---
+++ src/main/java/br/com/casasbahia/crud_h2/controller/ProdutoMapper.java
@@ -0,0 +1,31 @@
+package br.com.casasbahia.crud_h2.controller;
+
+import br.com.casasbahia.crud_h2.controller.dto.ProdutoRequest;
+import br.com.casasbahia.crud_h2.controller.dto.ProdutoResponse;
+import br.com.casasbahia.crud_h2.model.Produto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProdutoMapper {
+
+    public Produto toEntity(ProdutoRequest dto) {
+        Produto p = new Produto();
+        p.setNome(dto.getNome());
+        p.setNcm(dto.getNcm());
+        p.setDescricaoNcm(dto.getDescricaoNcm());
+        p.setPreco(dto.getPreco());
+        p.setQuantidade(dto.getQuantidade());
+        return p;
+    }
+
+    public ProdutoResponse toResponse(Produto produto) {
+        ProdutoResponse dto = new ProdutoResponse();
+        dto.setId(produto.getId());
+        dto.setNome(produto.getNome());
+        dto.setNcm(produto.getNcm());
+        dto.setDescricaoNcm(produto.getDescricaoNcm());
+        dto.setPreco(produto.getPreco());
+        dto.setQuantidade(produto.getQuantidade());
+        return dto;
+    }
+}
